bd61855e9abf7ba8beb4cf6a186b4424875db839
aplJake <apl.jake@gmail.com>
2019-11-02 10:48:59 +0200

    #0 initilized project

---
+++ VideoGallery/src/api/constants.js
@@ -0,0 +1,4 @@
+export const PRIVATE_ACCESS_KEY =
+  '563492ad6f9170000100000113b8c249f0c7457183f0c551aaf66c8b';
+export const API_URL =
+  'https://api.pexels.com/v1/search?query=example+query&per_page=15&page=1';
